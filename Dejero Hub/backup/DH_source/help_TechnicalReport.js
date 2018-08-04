20c14ad2ab433c8c2557c28c877e758027a513f7
faizalsomani <faizal9somani@gmail.com>
2018-08-04 06:57:18 +0530

    Delete help_TechnicalReport.js

--- Dejero Hub/backup/DH_source/help_TechnicalReport.js
+++
@@ -1,6 +0,0 @@
-(function (){
-    var link = document.createElement("a");
-    link.download = 'README';
-    link.href = 'https://system.na2.netsuite.com/core/media/media.nl?id=443&c=1783250&h=9fd3865d388b9c758d7a&_xt=.pdf';
-    link.click();
-}());
