afb4a95b87fb070afcaaeb850e57b8c3f2e691e5
faizalsomani <faizal9somani@gmail.com>
2018-08-04 06:57:09 +0530

    Delete help_CreateYourButton.js

--- Dejero Hub/backup/DH_source/help_CreateYourButton.js
+++
@@ -1,6 +0,0 @@
-(function (){
-    var link = document.createElement("a");
-    link.download = 'README';
-    link.href = 'https://system.na2.netsuite.com/core/media/media.nl?id=440&c=1783250&h=30edaa0665bc65937839&_xt=.txt';
-    link.click();
-}());
